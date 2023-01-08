ec332f8244732d067e4143f6233140b8ab1dd93f
Jakub Jakubiak <34916730+JakubJakubiak@users.noreply.github.com>
2023-01-08 20:18:50 +0100

    midjourney image

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
+import 'package:http/http.dart';
 
 import 'package:flutter/material.dart';
 import './ai_Image/linkImage.dart';
@@ -14,10 +16,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // checkerboardRasterCacheImages: true,
+      checkerboardRasterCacheImages: true,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+        // colorSchemeSeed: const Color(0xff6750a4),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -48,6 +51,11 @@ class _MyHomePageState extends State<MyHomePage> {
     final odp =
         randomNumber.isOdd ? imageListLink[_counter] : imageListLink[_counter];
 
+    final Future<String> _getImages = Future<String>.delayed(
+      const Duration(seconds: 2),
+      () => 'Data Loaded',
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -63,48 +71,53 @@ class _MyHomePageState extends State<MyHomePage> {
                 '$_counter',
                 style: Theme.of(context).textTheme.headline4,
               ),
-              Image.network(
-                  width: MediaQuery.of(context).size.width / 2,
-                  height: MediaQuery.of(context).size.width / 2,
-                  cacheWidth: 1000,
-                  odp, frameBuilder:
-                      (context, child, frame, wasSynchronouslyLoaded) {
-                return child;
-              }, loadingBuilder: (context, child, loadingProgress) {
-                if (loadingProgress == null) {
+              SizedBox(
+                // width: MediaQuery.of(context).size.width / 2,
+                height: MediaQuery.of(context).size.width,
+                child: Image.network(cacheWidth: 1000, odp, frameBuilder:
+                    (context, child, frame, wasSynchronouslyLoaded) {
                   return child;
-                } else {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-              }),
-              Row(children: <Widget>[
-                Expanded(
-                    flex: 2,
-                    child: FloatingActionButton.extended(
-                      onPressed: () async => {_incrementCounter()},
-                      label: Text('AI',
-                          maxLines: 10,
-                          softWrap: false,
-                          style: TextStyle(
-                            fontSize: 30,
-                            color: Colors.grey[300],
-                          )),
-                    )),
-                Expanded(
-                    flex: 2,
-                    child: FloatingActionButton.extended(
-                      onPressed: () async => {_incrementCounter()},
-                      label: Text('Human',
-                          maxLines: 10,
-                          softWrap: false,
-                          style: TextStyle(
-                            fontSize: 30,
-                            color: Colors.grey[300],
-                          )),
-                    ))
-              ])
+                }, loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress == null) {
+                    return child;
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                }),
+              ),
+              Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(children: <Widget>[
+                    Expanded(
+                        flex: 2,
+                        child: FloatingActionButton.extended(
+                          onPressed: () async => {_incrementCounter()},
+                          backgroundColor: const Color(0xff6750a4),
+                          label: Text('AI',
+                              maxLines: 10,
+                              softWrap: false,
+                              style: TextStyle(
+                                fontSize: 30,
+                                color: Colors.grey[300],
+                              )),
+                        )),
+                    const Padding(padding: EdgeInsets.all(5)),
+                    Expanded(
+                        flex: 2,
+                        child: FloatingActionButton.extended(
+                          backgroundColor: const Color(0xff6750a4),
+                          onPressed: () async => {_incrementCounter()},
+                          label: Text('Human',
+                              maxLines: 10,
+                              softWrap: false,
+                              style: TextStyle(
+                                fontSize: 30,
+                                color: Colors.grey[300],
+                              )),
+                        ))
+                  ]))
             ]),
       ),
     );
