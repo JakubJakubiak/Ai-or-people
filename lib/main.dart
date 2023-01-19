7e75d186a203f5863ad17956429f1042ed6d0bc3
Jakub Jakubiak <34916730+JakubJakubiak@users.noreply.github.com>
2023-01-19 23:21:49 +0100

    image size is detected

--- lib/main.dart
+++ lib/main.dart
@@ -55,7 +55,13 @@ class _MyHomePageState extends State<MyHomePage>
   int randomNumber = Random().nextInt(10);
   int _counter = 0;
   List<Map<String, dynamic>> categoryDraw = [];
+  Animation<double>? animation;
+  dynamic sizeW;
+  dynamic sizeH;
+
   late Timer timer;
+  double posValue = 1000.0;
+  double posValueHeight = 1000.0;
 
   void _incrementCounter() {
     setState(() {
@@ -89,11 +95,16 @@ class _MyHomePageState extends State<MyHomePage>
     if (categoryWin == categoryDraw[randomNumber]['category'] &&
         answer == false) _incrementCounter();
 
+    posValue = posValue == 0.0 ? sizeW : sizeW;
+    posValueHeight = posValueHeight == 0.0 ? sizeH : sizeH;
+
     (answer == false)
         ? timer = Timer(const Duration(milliseconds: 600), () {
             setState(() {
               answer = false;
               randomNumbers();
+              posValue = sizeW;
+              posValueHeight = sizeH;
             });
           })
         : null;
@@ -106,100 +117,128 @@ class _MyHomePageState extends State<MyHomePage>
     });
   }
 
+  Future<Size> _calculateImageDimension(images) async {
+    Completer<Size> completer = Completer();
+    Image image = Image.network(images);
+    image.image.resolve(const ImageConfiguration()).addListener(
+      ImageStreamListener(
+        (ImageInfo image, bool synchronousCall) {
+          var myImage = image.image;
+          Size size = Size(myImage.width.toDouble(), myImage.height.toDouble());
+          completer.complete(size);
+          sizeW = size.width;
+          sizeH = size.height;
+        },
+      ),
+    );
+    return completer.future;
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     loadImages();
-    String images =
-        categoryDraw[randomNumber]['link'] ?? categoryDraw[5]['link'];
+
+    String images = categoryDraw[randomNumber]['link'];
+    _calculateImageDimension(images).then((size) => {print(size)});
 
     return Scaffold(
         appBar: AppBar(
           title: Text(widget.title),
         ),
         body: Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'Guess which image was made by human and which by ai?',
-                  maxLines: 5,
-                ),
-                Text(
-                  '$_counter',
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.width,
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <
+              Widget>[
+            const Text(
+              'Guess which image was made by human and which by ai?',
+              maxLines: 5,
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            SizedBox(
+                height: MediaQuery.of(context).size.width,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  alignment: Alignment.center,
                   child: AnimatedSwitcher(
-                      duration: const Duration(milliseconds: 300),
-                      transitionBuilder:
-                          (Widget child, Animation<double> aniamtion) {
-                        return ScaleTransition(scale: aniamtion, child: child);
-                      },
-                      child: Image.network(
-                          cacheWidth: 1000,
-                          images,
-                          key: ValueKey<String>('$randomNumber'),
-                          fit: BoxFit.cover, frameBuilder:
-                              (context, child, frame, wasSynchronouslyLoaded) {
+                    duration: const Duration(milliseconds: 300),
+                    transitionBuilder:
+                        (Widget child, Animation<double> aniamtion) {
+                      return ScaleTransition(scale: aniamtion, child: child);
+                    },
+                    child: Image.network(
+                        cacheWidth: 1000,
+                        images,
+                        key: ValueKey<String>('$randomNumber'),
+                        fit: BoxFit.cover, frameBuilder:
+                            (context, child, frame, wasSynchronouslyLoaded) {
+                      return child;
+                    }, loadingBuilder: (context, child, loadingProgress) {
+                      if (loadingProgress == null) {
                         return child;
-                      }, loadingBuilder: (context, child, loadingProgress) {
-                        if (loadingProgress == null) {
-                          return child;
-                        } else {
-                          return const Center(
-                            child: CircularProgressIndicator(),
-                          );
-                        }
-                      })),
-                ),
-                Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: Row(children: <Widget>[
-                      Expanded(
-                          flex: 2,
-                          child: FloatingActionButton.extended(
-                            onPressed: () async => {
-                              HapticFeedback.mediumImpact(),
-                              trueOdFalse(context, categoryWin: 'ai'),
-                            },
-                            backgroundColor: answer == true
-                                ? categoryDraw[randomNumber]['category'] == 'ai'
-                                    ? const Color.fromARGB(255, 41, 95, 43)
-                                    : const Color.fromARGB(255, 129, 42, 42)
-                                : const Color(0xff6750a4),
-                            label: Text('AI',
-                                maxLines: 10,
-                                softWrap: false,
-                                style: TextStyle(
-                                  fontSize: 30,
-                                  color: Colors.grey[300],
-                                )),
-                          )),
-                      const Padding(padding: EdgeInsets.all(5)),
-                      Expanded(
-                          flex: 2,
-                          child: FloatingActionButton.extended(
-                            onPressed: () async => {
-                              HapticFeedback.mediumImpact(),
-                              trueOdFalse(context, categoryWin: 'human'),
-                            },
-                            backgroundColor: answer == true
-                                ? categoryDraw[randomNumber]['category'] ==
-                                        'human'
-                                    ? const Color.fromARGB(255, 41, 95, 43)
-                                    : const Color.fromARGB(255, 129, 42, 42)
-                                : const Color(0xff6750a4),
-                            label: Text('Human',
-                                maxLines: 10,
-                                softWrap: false,
-                                style: TextStyle(
-                                  fontSize: 30,
-                                  color: Colors.grey[300],
-                                )),
-                          ))
-                    ]))
-              ]),
+                      } else {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                    }),
+                  ),
+                )),
+            Padding(
+                padding: const EdgeInsets.all(10),
+                child: Row(children: <Widget>[
+                  Expanded(
+                      flex: 2,
+                      child: FloatingActionButton.extended(
+                        onPressed: () async => {
+                          HapticFeedback.mediumImpact(),
+                          trueOdFalse(context, categoryWin: 'ai'),
+                        },
+                        backgroundColor: answer == true
+                            ? categoryDraw[randomNumber]['category'] == 'ai'
+                                ? const Color.fromARGB(255, 41, 95, 43)
+                                : const Color.fromARGB(255, 129, 42, 42)
+                            : const Color(0xff6750a4),
+                        label: Text('AI',
+                            maxLines: 10,
+                            softWrap: false,
+                            style: TextStyle(
+                              fontSize: 30,
+                              color: Colors.grey[300],
+                            )),
+                      )),
+                  const Padding(padding: EdgeInsets.all(5)),
+                  Expanded(
+                      flex: 2,
+                      child: FloatingActionButton.extended(
+                        onPressed: () async => {
+                          HapticFeedback.mediumImpact(),
+                          trueOdFalse(context, categoryWin: 'human'),
+                        },
+                        backgroundColor: answer == true
+                            ? categoryDraw[randomNumber]['category'] == 'human'
+                                ? const Color.fromARGB(255, 41, 95, 43)
+                                : const Color.fromARGB(255, 129, 42, 42)
+                            : const Color(0xff6750a4),
+                        label: Text('Human',
+                            maxLines: 10,
+                            softWrap: false,
+                            style: TextStyle(
+                              fontSize: 30,
+                              color: Colors.grey[300],
+                            )),
+                      ))
+                ]))
+          ]),
         ));
   }
 }
